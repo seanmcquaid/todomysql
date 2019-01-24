8cd6c6e465288d7ff14e84b2bab82aab98737177
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-24 11:20:13 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'db1',
+  password : 'roasted',
+  database : 'restaurant_db'
+});
+ 
+connection.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
